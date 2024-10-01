2a0f6c04844866e32fe1b5edf86d781f0fe4db0d
Arun Raj <arungraj23@gmail.com>
2024-10-01 21:31:46 +0530

    Problem Num 242 minor modifications

--- 242_valid_anagram.py
+++ 242_valid_anagram.py
@@ -16,18 +16,33 @@ Output: false
 
 Constraints:
 
-1 <= s.length, t.length <= 5 * 104
+1 <= s.length, t.length <= 5 * 10^4
 s and t consist of lowercase English letters.
  
 
 Follow up: What if the inputs contain Unicode characters? How would you adapt your solution to such a case?"""
 
 
-def valid_anagram(s, t):
+from collections import Counter
+
+
+def valid_anagram(s: str, t: str) -> bool:
+    # Time Complexity: O(nlog n)
+    # Space Complexity: O(n)
     _s = sorted(s)
     _t = sorted(t)
     return (_s == _t)
 
 
+# More pythonic way
+
+def efficient_solution(s: str, t: str) -> bool:
+    # Time Complexity: O(n)
+    # Space Complexity: O(n)
+    return Counter(s) == Counter(t)
+
+
 print(valid_anagram("anagram", "nagaram"))
 print(valid_anagram("rat", "car"))
+print(efficient_solution("anagram", "nagaram"))
+print(efficient_solution("rat", "car"))
